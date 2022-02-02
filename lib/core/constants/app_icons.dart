ff35c03134bc184db9fa32f4e129067564d8dfad
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-02 20:01:08 +0500

    Illustrations and Icons were added

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,33 @@
+class AppIcons {
+  static const String back = 'assets/icons/back.svg';
+  static const String camera = 'assets/icons/camera.svg';
+  static const String card = 'assets/icons/card.svg';
+  static const String cart = 'assets/icons/cart.svg';
+  static const String clock = 'assets/icons/clock.svg';
+  static const String close = 'assets/icons/close.svg';
+  static const String delivery = 'assets/icons/delivery.svg';
+  static const String document = 'assets/icons/document.svg';
+  static const String done = 'assets/icons/done.svg';
+  static const String facebook = 'assets/icons/facebook.svg';
+  static const String fastDelivery = 'assets/icons/fast-delivery.svg';
+  static const String fire = 'assets/icons/fire.svg';
+  static const String food = 'assets/icons/food.svg';
+  static const String forward = 'assets/icons/forward.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String invisible = 'assets/icons/invisible.svg';
+  static const String location = 'assets/icons/location.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String logout = 'assets/icons/logout.svg';
+  static const String market = 'assets/icons/market.svg';
+  static const String minus = 'assets/icons/minus.svg';
+  static const String notify = 'assets/icons/notify.svg';
+  static const String order = 'assets/icons/order.svg';
+  static const String phone = 'assets/icons/phone.svg';
+  static const String plus = 'assets/icons/plus.svg';
+  static const String profile = 'assets/icons/profile.svg';
+  static const String rating = 'assets/icons/rating.svg';
+  static const String recommended = 'assets/icons/recommended.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String share = 'assets/icons/share.svg';
+  static const String visible = 'assets/icons/visible.svg';
+}
---
+++ lib/core/constants/app_illustrations.dart
@@ -0,0 +1,5 @@
+class AppIllustrations {
+  static const String illustration1 = 'assets/illustrations/illustration1.svg';
+  static const String illustration2 = 'assets/illustrations/illustration2.svg';
+  static const String illustration3 = 'assets/illustrations/illustration3.svg';
+}
