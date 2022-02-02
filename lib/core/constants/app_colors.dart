f3446ebe6c78841625b7e9532a4085c8f108e00a
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-02 16:03:44 +0500

    Colors and icons were added

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color green = Color(0xFF22A45D);
+  static const Color dark = Color(0xFF010F07);
+  static const Color darkGrey = Color(0xFF868686);
+  static const Color grey = Color(0xFFE3E3E3);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color orange = Color(0xFFEF9920);
+  static const Color blue = Color(0xFF4285F4);
+  static const Color indigo = Color(0xFF395998);
+}
