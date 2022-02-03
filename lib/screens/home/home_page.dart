e601c73ef2f2ca1b535868864b8e9eafdf68e456
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-03 23:45:43 +0500

    Fonts were added

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,43 +3,21 @@ import 'package:foodly/core/components/exporting_packages.dart';
 import 'package:foodly/cubit/home/home_page_cubit.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final FocusNode _focusNode = FocusNode();
+  late BuildContext _context;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (_)=> HomePageCubit(),
-    child: BlocBuilder<HomePageCubit, HomePageState>(builder: (context, state) {
-      return Scaffold(
-        body: Center(
-
-          child: Wrap(
-            children: [
-              _buildMySizedBox(),
-              _buildMySizedBox(),
-              _buildMySizedBox(),
-              _buildMySizedBox(),
-            ],
-          ),
-        ),
-      );
-    },),
+    _context = context;
+    return BlocProvider(
+      create: (_) => HomePageCubit(),
+      child: BlocBuilder<HomePageCubit, HomePageState>(
+        builder: (context, state) {
+          return Scaffold();
+        },
+      ),
     );
-  }
-
-  MySizedBox _buildMySizedBox() {
-    return MySizedBox(
-              width: 65.0,
-              child: TextField(
-                textInputAction: TextInputAction.next,
-                onChanged: (v) {
-
-                },
-                maxLength: 1,
-                textAlign: TextAlign.center,
-                autofocus: true,
-                keyboardType: TextInputType.number,
-                maxLengthEnforcement: MaxLengthEnforcement.enforced,
-              ),
-            );
   }
 }
---
+++ lib/widgets/buttons/social_media_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaButton extends StatelessWidget {
+  const SocialMediaButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: (){}, child: Text(''));
+  }
+}
