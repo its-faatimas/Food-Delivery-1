e9300d29a74305b4571f7f61e016e0a32306de0c
its-faatimas <yourgitemail@example.com>
2022-02-04 15:56:42 +0500

    Social Media Button added

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foodly/core/components/exporting_packages.dart';
 import 'package:foodly/cubit/home/home_page_cubit.dart';
+import 'package:foodly/widgets/buttons/social_media_button.dart';
 import 'package:foodly/widgets/my_app_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,7 +20,11 @@ class HomePage extends StatelessWidget {
               // Child'ga Padding berdik
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: MyIconButton(onPressed: () {}, assetIcon: AppIcons.food),
+                child: SocialMediaButton(
+                  onPressed: () {},
+                  assetIcon: AppIcons.facebook,
+                  label: "Connect with Facebook".toUpperCase(),
+                ),
               ),
             ),
           );
