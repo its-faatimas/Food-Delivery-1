786006ea17231a9dc9198676a2d1763b9ca6bf71
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-09 13:56:10 +0500

    PaymentMethodsPage is ready

--- lib/screens/settings/payment_methods_page.dart
+++ lib/screens/settings/payment_methods_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foodly/core/components/exporting_packages.dart';
+import 'package:foodly/widgets/buttons/my_outlined_button.dart';
+
 class PaymentMethodsPage extends StatelessWidget {
   const PaymentMethodsPage({Key? key}) : super(key: key);
 
@@ -7,6 +9,18 @@ class PaymentMethodsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SimpleAppBar(title: LocaleKeys.payment_methods.tr()),
+      body: Padding(
+        padding: MyEdgeInsets.symmetric(h: 44.0, v: 100.0),
+        child: MethodInfo(
+          illustration: AppIllustrations.creditCard,
+          title: LocaleKeys.payment_methods,
+          subtitle: LocaleKeys.enter_email_to_reset,
+          widget: MyOutlinedButton(
+            onPressed: () {},
+            label: LocaleKeys.add_your_credit,
+          ),
+        ),
+      ),
     );
   }
 }
